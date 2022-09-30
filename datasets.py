6c54f236f43628749d0c8ba75f1cca543503bad1
Peter Frendl <pfre00@gmail.com>
2022-09-30 21:59:30 +0200

    added uniform split and quadrant split experiments

--- datasets.py
+++ datasets.py
@@ -1,20 +1,24 @@
+from typing import Callable
+
 import torch
 from torch import Tensor
 from torch.utils.data import Dataset
 
 
 class PixelRegressionDataset(Dataset[tuple[Tensor, Tensor]]):
-    def __init__(self, size: int) -> None:
+    def __init__(self, size: int, mask: Tensor) -> None:
         super().__init__()
         self.size = size
+        gi, gj = torch.meshgrid(torch.arange(size), torch.arange(size), indexing="ij")
+        grid = torch.stack([gi, gj], dim=2)
+        self.samples = grid[mask]
 
     def __len__(self) -> int:
-        return self.size**2
+        return len(self.samples)
 
     def __getitem__(self, index: int) -> tuple[Tensor, Tensor]:
-        i = index // self.size
-        j = index % self.size
-        target = 2 * torch.tensor([i, j]) / (self.size - 1) - 1
+        sample = self.samples[index]
+        target = 2 * sample / (self.size - 1) - 1
         img = torch.zeros((3, self.size, self.size))
-        img[:, i, j] = 1.0
+        img[:, sample[0], sample[1]] = 1.0
         return img, target
--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 from pathlib import Path
 
-import enlighten
 import matplotlib.pyplot as plt
 import torch
+from enlighten import Manager
+from torch import Tensor
 from torch.utils.data import DataLoader
 
 from datasets import PixelRegressionDataset
 from models import Regressor0, Regressor1, Regressor2
 
 
-def main() -> None:
-    num_epochs = 80
-    batch_size = 32
-    device = torch.device("cuda")
+grid_size = 32
+num_epochs = 80
+batch_size = 32
+device = torch.device("cuda")
 
-    out_dir = Path("outputs")
-    out_dir.mkdir(exist_ok=True)
 
+def experiment(train_mask: Tensor, test_mask: Tensor, manager: Manager, save_path: Path) -> None:
     fig = plt.figure(figsize=(15, 10), constrained_layout=True)
     axes = fig.subplot_mosaic(
         """
@@ -30,26 +30,24 @@ def main() -> None:
         (Regressor2, axes["C"], "CoordConv at each layer"),
     ]
 
-    manager = enlighten.Manager()
-
-    test_bar = manager.counter(total=len(tests), desc="Test progress", unit="models", leave=False)
+    experiment_pbar = manager.counter(total=len(tests), desc="Experiment progress", unit="models", leave=False)
     for model_ctr, ax, description in tests:
-        dataset = PixelRegressionDataset(size=32)
-        assert len(dataset) % batch_size == 0
+        trainset = PixelRegressionDataset(size=grid_size, mask=train_mask)
+        testset = PixelRegressionDataset(size=grid_size, mask=test_mask)
 
         train_data_loader = DataLoader(
-            dataset=dataset, batch_size=batch_size, shuffle=True, num_workers=2, persistent_workers=True
+            dataset=trainset, batch_size=batch_size, shuffle=True, num_workers=2, persistent_workers=True
         )
-        test_data_loader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=False, num_workers=2)
+        test_data_loader = DataLoader(dataset=testset, batch_size=batch_size, shuffle=False, num_workers=2)
 
         model = model_ctr().to(device)
         optimizer = torch.optim.RAdam(params=model.parameters(), lr=0.001)
 
         avg_losses = []
-        train_bar = manager.counter(total=num_epochs, desc="Training progress", unit="epochs", leave=False)
+        train_pbar = manager.counter(total=num_epochs, desc="Training progress", unit="epochs", leave=False)
         for _ in range(num_epochs):
             losses = []
-            epoch_bar = manager.counter(total=len(train_data_loader), desc="Epoch progress", unit="iters", leave=False)
+            epoch_pbar = manager.counter(total=len(train_data_loader), desc="Epoch progress", unit="iters", leave=False)
             for img, target in train_data_loader:
                 img, target = img.to(device), target.to(device)
 
@@ -61,35 +59,45 @@ def main() -> None:
                 optimizer.step()
 
                 losses.append(loss.item())
-                epoch_bar.update()
-            epoch_bar.close()
+                epoch_pbar.update()
+            epoch_pbar.close()
 
             avg_loss = sum(losses) / len(losses)
             avg_losses.append(avg_loss)
-            train_bar.update()
-        train_bar.close()
+            train_pbar.update()
+        train_pbar.close()
 
-        targets = []
+        train_targets = []
+        test_targets = []
         preds = []
         model.eval()
+        for img, target in train_data_loader:
+            img, target = img.to(device), target.to(device)
+            with torch.no_grad():
+                pred = model(img)
+            train_targets.append(target)
+            preds.append(pred)
         for img, target in test_data_loader:
             img, target = img.to(device), target.to(device)
             with torch.no_grad():
                 pred = model(img)
-            targets.append(target)
+            test_targets.append(target)
             preds.append(pred)
-        targets_np = torch.cat(targets, dim=0).cpu().numpy()
+        train_targets_np = torch.cat(train_targets, dim=0).cpu().numpy()
+        test_targets_np = torch.cat(test_targets, dim=0).cpu().numpy()
         preds_np = torch.cat(preds, dim=0).cpu().numpy()
 
         ax.set_title(description)
         ax.set_xlabel("Dimension 1")
         ax.set_ylabel("Dimension 2")
-        ax.scatter(x=targets_np[:, 0], y=targets_np[:, 1], color="green", s=25.0)
-        ax.scatter(x=preds_np[:, 0], y=preds_np[:, 1], color="red", s=12.5)
+        ax.scatter(x=train_targets_np[:, 0], y=train_targets_np[:, 1], color="green", s=25.0, label="Train set")
+        ax.scatter(x=test_targets_np[:, 0], y=test_targets_np[:, 1], color="blue", s=25.0, label="Test set")
+        ax.scatter(x=preds_np[:, 0], y=preds_np[:, 1], color="red", s=12.5, label="Predictions")
+        ax.legend()
         axes["D"].plot(avg_losses, label=description)
 
-        test_bar.update()
-    test_bar.close()
+        experiment_pbar.update()
+    experiment_pbar.close()
 
     axes["D"].grid(True)
     axes["D"].legend()
@@ -97,8 +105,32 @@ def main() -> None:
     axes["D"].set_ylabel("L2 loss")
     fig.suptitle("Pixel coordinate regression")
 
-    plt.savefig(out_dir / f"test.png")
+    plt.savefig(save_path)
     plt.close(fig=fig)
+
+
+def main() -> None:
+    out_dir = Path("outputs")
+    out_dir.mkdir(exist_ok=True)
+    manager = Manager()
+
+    overfit_mask = torch.ones((grid_size, grid_size), dtype=torch.bool)
+    uniform_split_mask = torch.rand((grid_size, grid_size)) < 0.75
+    quadrant_split = torch.ones((grid_size, grid_size), dtype=torch.bool)
+    quadrant_split[grid_size // 2 :, grid_size // 2 :] = False
+
+    train_test_masks: list[tuple[Tensor, Tensor, str]] = [
+        (overfit_mask, overfit_mask, "Overfit grid"),
+        (uniform_split_mask, ~uniform_split_mask, "Uniform split"),
+        (quadrant_split, ~quadrant_split, "Quadrant split"),
+    ]
+
+    test_pbar = manager.counter(total=len(train_test_masks), desc="Test progress", unit="experiments", leave=False)
+    for train_mask, test_mask, experiment_name in train_test_masks:
+        save_name = experiment_name.lower().replace(" ", "_")
+        experiment(train_mask=train_mask, test_mask=test_mask, manager=manager, save_path=out_dir / f"{save_name}.png")
+        test_pbar.update()
+    test_pbar.close()
 
     manager.stop()
 
